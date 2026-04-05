b2a91ee37994c43ee6e5120bf9787c02c30682ea
Dann <daniel.villarruel@yachaytech.edu.ec>
2026-04-05 14:53:56 -0500

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://dvillarruelyanez.github.io',
+    site: 'https://dvillarruelyanez.github.io'
 });
